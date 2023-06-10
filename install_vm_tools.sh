8a9d9b6f885b5fea51cfdda9360ae3244fef23ac
gedemais <geoffrey.demaiso@gmail.com>
2023-06-10 10:58:36 +0200

    problems fixed up. Let's rebase on master

--- install_vm_tools.sh
+++ install_vm_tools.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
+#
 # Update of local apt
 sudo apt update
 # Installation of needed packages
 sudo apt install virtualbox
 sudo apt install vagrant
-
-# Launch of VM
-vagrant up --provision
-# SSH connection to VM
-vagrant ssh
